e5e25c31b54c875d43395f0d9cb58c35a4e9a0cd
DreMind5000 <89564302+DreMind5000@users.noreply.github.com>
2021-10-16 03:43:03 -0400

    Updates

---
+++ precache-manifest.029cd89f2c0663474425dc8daadb7cd1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80bcd0358ea39ff96ff3ea1ddc2d35ef",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f45315e3fc61037fe9da",
+    "url": "./static/css/2.a6a636a0.chunk.css"
+  },
+  {
+    "revision": "b7738b8c4d4bd6e28885",
+    "url": "./static/css/main.3effe839.chunk.css"
+  },
+  {
+    "revision": "f45315e3fc61037fe9da",
+    "url": "./static/js/2.e4d88875.chunk.js"
+  },
+  {
+    "revision": "21b2dd38606ac28751db0d936591a095",
+    "url": "./static/js/2.e4d88875.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7738b8c4d4bd6e28885",
+    "url": "./static/js/main.6e3a2f16.chunk.js"
+  },
+  {
+    "revision": "e720711dd3e4e8b209d0",
+    "url": "./static/js/runtime-main.43c7aded.js"
+  },
+  {
+    "revision": "d5322ee66c09fb9ab1b37a499b62d6d9",
+    "url": "./static/media/Binance-Icon-Logo.wine.d5322ee6.svg"
+  },
+  {
+    "revision": "2565884a03843bf5f37bf4a806402acf",
+    "url": "./static/media/avalanche-avax-logo.2565884a.png"
+  },
+  {
+    "revision": "7df580b589eb21f94c68c9fae7461977",
+    "url": "./static/media/file_upload.7df580b5.svg"
+  },
+  {
+    "revision": "50ef9f2c3d2ab7649be46cb917b2a74f",
+    "url": "./static/media/file_upload_hero.50ef9f2c.svg"
+  },
+  {
+    "revision": "2410baa7a2e708e2952f53672b677683",
+    "url": "./static/media/logo.2410baa7.png"
+  },
+  {
+    "revision": "a0103549b5dca7100830a06fc1a09cb9",
+    "url": "./static/media/metamask.a0103549.svg"
+  },
+  {
+    "revision": "9d3902825cff4deebbf2eae0a0f37df1",
+    "url": "./static/media/polygon-logo.9d390282.svg"
+  }
+]);
